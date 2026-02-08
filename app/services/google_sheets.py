d037cb8911856309eadfdc5c99c6d016892347a0
NewbeeTryToCode <asurascientist@gmail.com>
2026-02-08 21:11:50 +0700

    fixing google credentials setup v3

--- app/services/google_sheets.py
+++ app/services/google_sheets.py
@@ -2,7 +2,7 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from app.core.config import settings
 import json
-
+import Path
 class GoogleSheetsService:
     def __init__(self):
         scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
